c60ed690158b466b5f607f7a642abd36e069cd02
Wilson Delgado <wilsondelgadomoreno@gmail.com>
2024-12-06 10:45:49 -0500

    1 ejercicio

--- Java_Script_Ejercicios/Ejercicios1.js
+++ Java_Script_Ejercicios/Ejercicios1.js
@@ -1,2 +1,21 @@
 // Escribir un programa que pregunte al usuario su edad y muestre por pantalla si es mayor o 
 // menor de edad, el programa debe validar que solo se puedan ingresar números positivos. 
+
+function esNumeroPositivo(valor){
+    const numero = Number(valor);
+    return !isNaN(numero) && numero >0;
+}
+
+function verificarEdad() {
+    let edad=prompt("ingrese su edad");
+    while (!esNumeroPositivo(edad)) {
+        edad = prompt("Entrada invalida ¡Por favor! ingresa un numero positivo  para tu edad");
+    }
+    edad = Number(edad);
+    if (edad>=18) {
+        alert("Eres mayor de Edad")
+    }else{
+        alert("Eres menor de edad")
+    }
+}
+verificarEdad();
