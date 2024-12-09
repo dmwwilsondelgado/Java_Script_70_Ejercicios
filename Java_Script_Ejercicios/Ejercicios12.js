7211ee6650df0a00c449cdaf05ae00aa43bc7aa8
Wilson Delgado <wilsondelgadomoreno@gmail.com>
2024-12-09 09:06:33 -0500

    Esjercicio12

--- Java_Script_Ejercicios/Ejercicios12.js
+++ Java_Script_Ejercicios/Ejercicios12.js
@@ -1,1 +1,24 @@
 // Escribir un programa que calcule el área de un rectángulo:
+// -----------------------------
+alert("Calcular el área del triángulo");
+let base = prompt('Ingrese la base del triángulo');
+let altura = prompt('Ingrese la altura del triángulo');
+
+const calcularAreaTri = function(base, altura) {
+    try {
+        base = parseFloat(base);
+        altura = parseFloat(altura);
+        if (isNaN(base) || isNaN(altura)) {
+            throw new Error("Error: ambos valores deben ser números válidos.");
+        }
+        if (base <= 0 || altura <= 0) {
+            throw new Error("Error: la base y la altura deben ser mayores que cero.");
+        }
+        const area = (base * altura) / 2;
+        alert(`El área del triángulo con base ${base} y altura ${altura} es: ${area}`);
+
+    } catch (error) {
+        alert(error.message);
+    }
+}
+calcularAreaTri(base,altura);
