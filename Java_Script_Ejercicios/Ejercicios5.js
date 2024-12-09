352d5713ab3389b0e7624e08852bfe27bbf56fd4
Wilson Delgado <wilsondelgadomoreno@gmail.com>
2024-12-09 07:09:47 -0500

    Ejercicio5

--- Java_Script_Ejercicios/Ejercicios5.js
+++ Java_Script_Ejercicios/Ejercicios5.js
@@ -2,3 +2,13 @@
 // ingresos iguales o superiores a 1000 mensuales, escribir un programa que pregunte al 
 // usuario su edad e ingresos mensuales y muestre por pantalla si el usuario debe tributar o 
 // no.
+
+
+let edad = parseInt(prompt('Ingrese La edad'));
+let ingresos = parseInt(prompt('Ingrese el total de sus ingresos mensuales: '));
+
+let impuesto = function(edad,ingresos) {
+    if (edad > 16 && ingresos >= 1000) alert('El usuario SI tiene que tributar impuestos');
+    else alert('El usuario NO debe tributar impuestos');
+}
+impuesto(edad,ingresos);
