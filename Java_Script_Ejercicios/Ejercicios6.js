4aced8f215cbc0a04c7b33b6ee8f485aa69b02ac
Wilson Delgado <wilsondelgadomoreno@gmail.com>
2024-12-09 07:14:14 -0500

    Ejercicio7

--- Java_Script_Ejercicios/Ejercicios6.js
+++ Java_Script_Ejercicios/Ejercicios6.js
@@ -2,20 +2,3 @@
 // nombre, el grupo A está formado por las mujeres con un nombre anterior a la M y los 
 // hombres con un nombre posterior a la N y el grupo B por el resto, escribir un programa que 
 // pregunte al usuario su nombre y sexo y muestre por pantalla el grupo al que corresponde.
-let renta = parseInt(prompt('Ingrese su renta anual'));
-
-function renta_anual(renta) {
-    if (renta >= 10000 && renta <= 20000) {
-        alert('El impositivo que le corresponde es del 5%');
-    }
-    if (renta >= 20000 && renta <= 35000) {
-        alert('El impositivo que le corresponde es del 10%');
-    }
-    if (renta >= 35000 && renta <= 60000) {
-        alert('El impositivo que le corresponde es del 20%');
-    }
-    if (renta > 60000) {
-        alert('El impositivo que le corresponde es del 45%');
-    }
-}
-renta_anual(renta);
--- Java_Script_Ejercicios/Ejercicios7.js
+++ Java_Script_Ejercicios/Ejercicios7.js
@@ -5,3 +5,21 @@
 // d. más de 60000 ---- 45%
 // escribir un programa que pregunte al usuario su renta anual y muestre por pantalla el tipo 
 // de impositivo que le corresponde
+
+let renta = parseInt(prompt('Ingrese su renta anual'));
+
+function renta_anual(renta) {
+    if (renta >= 10000 && renta <= 20000) {
+        alert('El impositivo que le corresponde es del 5%');
+    }
+    if (renta >= 20000 && renta <= 35000) {
+        alert('El impositivo que le corresponde es del 10%');
+    }
+    if (renta >= 35000 && renta <= 60000) {
+        alert('El impositivo que le corresponde es del 20%');
+    }
+    if (renta > 60000) {
+        alert('El impositivo que le corresponde es del 45%');
+    }
+}
+renta_anual(renta);
