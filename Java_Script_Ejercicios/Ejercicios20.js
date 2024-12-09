7d4aa37a5a64f39691c495bdf32f67d47bb2dea1
Wilson Delgado <wilsondelgadomoreno@gmail.com>
2024-12-09 11:34:26 -0500

    ejercicio20.5

--- Java_Script_Ejercicios/Ejercicios20.js
+++ Java_Script_Ejercicios/Ejercicios20.js
@@ -1,2 +1,23 @@
 // Escriba un algoritmo que permita obtener las raíces reales de la ecuación de segundo 
 // grado: a * x2 + b + x + c, siendo X un valor constante
+
+function algoritmo(a, b, c, v) {
+    const resultado = (a * x2 + b + x + c);
+    return resultado;
+}
+
+function llamar() {
+    const a = parseFloat(prompt("Introduce el valor de a:"));
+    const b = parseFloat(prompt("Introduce el valor de b:"));
+    const c = parseFloat(prompt("Introduce el valor de c:"));
+    const v = parseFloat(prompt("Introduce el valor de v:"));
+
+    if (isNaN(a) || isNaN(b) || isNaN(c) || isNaN(v)) {
+        alert("Por favor, introduce números válidos.");
+        return;
+    }
+    const resultado = evaluarExpresion(a, b, c, v);
+    
+    alert(`El resultado de la expresión es: ${resultado}`);
+}
+llamar();
