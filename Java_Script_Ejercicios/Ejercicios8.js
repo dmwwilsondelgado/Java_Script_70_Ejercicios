84f92557852bce5b1dd9522f66fddc9ad23b7c72
Wilson Delgado <wilsondelgadomoreno@gmail.com>
2024-12-09 07:15:59 -0500

    Ejercicio8

--- Java_Script_Ejercicios/Ejercicios8.js
+++ Java_Script_Ejercicios/Ejercicios8.js
@@ -8,3 +8,28 @@
 // b. Nivel Puntuación Inaceptable 0.0 Aceptable 0.4 Meritorio 0.6 o más 
 // Escribir un programa que lea la puntuación del usuario e indique su nivel de rendimiento, 
 // así como la cantidad de dinero que recibirá el usuario.
+
+
+let puntos = parseFloat(prompt('Ingrese su puntuación '));
+let empleados = function(puntos) {
+    if (puntos === 0.0 || puntos === 0.4 || puntos === 0.6) {
+        if (puntos === 0.0) {
+            dinero = puntos * 2400;
+            pago = dinero + 2400;
+            alert(`Su nivel de rendimiento es INACEPTABLE ${puntos}, por lo que su pago es ${pago}€`);
+        }
+        if (puntos === 0.4) {
+            dinero = puntos * 2400;
+            pago = dinero + 2400;
+            alert(`Su nivel de rendimiento es ACEPTABLE ${puntos}, por lo que su pago es ${pago}€`);
+        }
+        if (puntos === 0.6) {
+            dinero = puntos * 2400;
+            pago = dinero + 2400;
+            alert(`Su nivel de rendimiento es MERITORIO ${puntos}, por lo que su pago es ${pago}€`);
+        }
+    } else {
+        alert('No se aceptan valores diferentes o intermedios');
+    }
+}
+empleados(puntos);
