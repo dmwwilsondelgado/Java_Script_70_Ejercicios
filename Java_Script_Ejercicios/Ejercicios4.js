d5a05dcb8b3b5a30938afc3da2547cc5ff41849a
Wilson Delgado <wilsondelgadomoreno@gmail.com>
2024-12-09 07:08:07 -0500

    Ejercicio4

--- Java_Script_Ejercicios/Ejercicios4.js
+++ Java_Script_Ejercicios/Ejercicios4.js
@@ -1,2 +1,16 @@
 // Escribir un programa que pida al usuario un número entero y muestre por pantalla si es par 
 // o impar.
+
+let numero = parseInt(prompt("Ingrese un numero entero"));
+
+const esPar = function(numero) {
+    if (numero >= 0) {
+        numero % 2 === 0 ? alert(`El numero ${numero} es par`) : alert(`El valor ingresado ${numero} es impar`);
+    } else if (numero < 0){
+        alert('No se acepta negativos');
+    }
+    else{
+        alert('Error ingrese un valor');
+    }
+}
+esPar(numero);
