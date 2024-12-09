dc8dd37a60a17fa758a0ba14e6178cb39db0a432
Wilson Delgado <wilsondelgadomoreno@gmail.com>
2024-12-09 09:24:41 -0500

    Ejercicio13

--- Java_Script_Ejercicios/Ejercicios13.js
+++ Java_Script_Ejercicios/Ejercicios13.js
@@ -1,1 +1,15 @@
 // Escribir un programa que lea 4 números y calcule la media.
+// -----------------------------a
+let numeros = [];
+for (let i = 1; i <= 4;i++) {
+    let numero = parseFloat(prompt(`ingrese el numero ${i}:`));
+    numeros.push(numero)
+}
+
+let suma = 0;
+for (let i = 0; i < numeros.length; i++) {
+    suma +=  numeros[i];
+}
+let media = suma /numeros.length;
+
+alert(`la Media de los numeros ingresados en la array es : ${media}`);
