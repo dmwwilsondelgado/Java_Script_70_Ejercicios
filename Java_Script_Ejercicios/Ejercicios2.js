e850a411dc194d48eccb9182f969bc7de995a112
Wilson Delgado <wilsondelgadomoreno@gmail.com>
2024-12-09 07:03:01 -0500

    Ejercicio2

--- Java_Script_Ejercicios/Ejercicios2.js
+++ Java_Script_Ejercicios/Ejercicios2.js
@@ -2,3 +2,22 @@
 // pregunte al usuario por la contraseña e imprima por pantalla si la contraseña introducida
 // por el usuario coincide con la guardada en la variable sin tener en cuenta mayúsculas y 
 // minúsculas, se debe validar que solo se pueda ingresar valores alfanuméricos.
+
+let ingresar_contrasena = prompt('Ingrese la contraseña');
+let contrasena = 'dmw';
+let regex = /^(\w)+$/i;
+
+const expresion = (ingresar_contrasena,contrasena)=>{
+    if (regex.test(ingresar_contrasena)) {       
+        if (contrasena === ingresar_contrasena) {
+            alert('La contraseña ingresada SI coincide con la contraseña guardada');
+        } else {
+            alert('La contraseña ingresada NO coincide con la contraseña guardada');
+        }
+    }
+    else{
+        alert('Error, ingrese valores alfanumericos');
+    }
+}
+let respuesta = expresion(ingresar_contrasena, contrasena);
+console.log(respuesta);
