4c719069796b3ac49342e84d0d151b7c7c64676e
Wilson Delgado <wilsondelgadomoreno@gmail.com>
2024-12-09 09:30:33 -0500

    Ejercicio14

--- Java_Script_Ejercicios/Ejercicios14.js
+++ Java_Script_Ejercicios/Ejercicios14.js
@@ -1,1 +1,23 @@
 // Escribir un programa que calcule el área de un triángulo.
+// Función para calcular el área del triángulo
+const calcularAreaTriangulo = function() {
+    let base = prompt('Ingrese la base del triángulo:');
+    let altura = prompt('Ingrese la altura del triángulo:');
+    try {
+        base = parseFloat(base);
+        altura = parseFloat(altura)
+        if (isNaN(base) || isNaN(altura)) {
+            throw new Error("Error: ambos valores deben ser números válidos.");
+        }
+        if (base <= 0 || altura <= 0) {
+            throw new Error("Error: la base y la altura deben ser mayores que cero.");
+        }
+        const area = (base * altura) / 2;     
+        alert(`El área del triángulo con base ${base} y altura ${altura} es: ${area}`);
+        
+    } catch (error) {
+        alert(error.message);
+    }
+}
+
+calcularAreaTriangulo();
