3e2a7fb3a23f76a3313f8dac87c3c49d94eefd3d
Wilson Delgado <wilsondelgadomoreno@gmai.com>
2024-12-12 18:22:30 -0500

    Ejercicio21

--- Java_Script_Ejercicios/Ejercicios21.js
+++ Java_Script_Ejercicios/Ejercicios21.js
@@ -1,1 +1,13 @@
 // Escribir un programa que calcule el volumen de un elipsoide.
+function calcularVolu() {
+    let radioA = prompt("Ingrese el radio a del elipsoide:");
+    let radioB = prompt("Ingrese el radio b del elipsoide:");
+    let radioC = prompt("Ingrese el radio c del elipsoide:");
+    radioA = parseFloat(radioA);
+    radioB = parseFloat(radioB);
+    radioC = parseFloat(radioC);
+    const volumen = (4/5) * Math.PI * radioA * radioB * radioC;
+    alert(`El volumen del elipsoide es ${volumen.toFixed(2)} unidades cúbicas.`);
+}
+
+calcularVolu();
