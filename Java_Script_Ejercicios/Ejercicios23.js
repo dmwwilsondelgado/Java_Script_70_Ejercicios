3f4d9aedacd2117635d4f7146b0f89af93191a98
Wilson Delgado <wilsondelgadomoreno@gmai.com>
2024-12-12 18:29:25 -0500

    Ejercicio23

--- Java_Script_Ejercicios/Ejercicios23.js
+++ Java_Script_Ejercicios/Ejercicios23.js
@@ -1,2 +1,8 @@
 // Realice un algoritmo que, a partir de proporcionarle la velocidad de un automóvil 
 // expresada en kilómetros por hora, proporcione la velocidad en metros por segundo.
+function velocidadKmh() {
+    const velocidadKmh = parseFloat(prompt("Ingrese la velocidad del automóvil en km/h:"));
+    const velocidadMs = velocidadKmh * (1000 / 3600);
+    alert(`La velocidad de l automóvil es ${velocidadMs.toFixed(2)} metros por segundo.`);
+}
+velocidadKmh();
