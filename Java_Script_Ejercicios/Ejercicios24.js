a0b245c75c4b060b43cbfd1fa4b4b52ca34c6a28
Wilson Delgado <wilsondelgadomoreno@gmai.com>
2024-12-12 18:43:23 -0500

    Ejercicio24

--- Java_Script_Ejercicios/Ejercicios24.js
+++ Java_Script_Ejercicios/Ejercicios24.js
@@ -1,2 +1,14 @@
 // Escriba una expresión regular que reconozca los números en punto flotante (por ejemplo -
 //     2.3e-1, -3e2, 23, 3.2)
+function verificFlotante() {
+    const numero = prompt("Ingrese un número para verificar si es un número en punto flotante:");
+    const regex = /^[+-]?\d*\.?\d+(e[+-]?\d+)?$/i;
+    const esFlotante = regex.test(numero);
+
+    if (esFlotante) {
+        alert(`${numero} es un número en punto flotante válido.`);
+    } else {
+        alert(`${numero} no es un número en punto flotante válido.`);
+    }
+}
+verificFlotante();
