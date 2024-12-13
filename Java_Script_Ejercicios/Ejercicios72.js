82a7fdb7132f1fda27625b668c64f7d467766583
Wilson Delgado <wilsondelgadomoreno@gmai.com>
2024-12-13 17:56:10 -0500

    Ejercicio72

--- Java_Script_Ejercicios/Ejercicios72.js
+++ Java_Script_Ejercicios/Ejercicios72.js
@@ -1,3 +1,8 @@
 // Escribir un programa que pida al usuario una palabra y luego muestre por pantalla una a 
 // una las letras de la palabra introducida empezando por la última.
 // // 
+let palabra = prompt("Introduce una palabra:");
+
+for (let i = palabra.length - 1; i >= 0; i--) {
+  alert(palabra[i]);
+}
