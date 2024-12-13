0c62d9f1e535a828bbca2db54860be927948779a
Wilson Delgado <wilsondelgadomoreno@gmai.com>
2024-12-13 17:46:06 -0500

    Ejercicio59

--- Java_Script_Ejercicios/Ejercicios59.js
+++ Java_Script_Ejercicios/Ejercicios59.js
@@ -1,3 +1,19 @@
 // . En un colegio, se necesita hacer una selección de basquetbol que represente al colegio, 
 // para lo cual es requisito indispensable que los postulantes sean menores o iguales a 19 
 // años, con una estatura de más de 175 cm y el peso tiene que estar entre los 80 kg y 75
+function seleccionarJugador() {
+    // Solicitar los datos del postulante
+    let edad = parseInt(prompt("Ingrese su edad:"));
+    let estatura = parseFloat(prompt("Ingrese su estatura en cm:"));
+    let peso = parseFloat(prompt("Ingrese su peso en kg:"));
+  
+    // Verificar si cumple con los requisitos
+    if (edad <= 19 && estatura > 175 && peso >= 75 && peso <= 80) {
+      alert("¡Felicidades! Cumples con los requisitos para ser parte de la selección de basquetbol.");
+    } else {
+      alert("Lo siento, no cumples con los requisitos para ser parte de la selección de basquetbol.");
+    }
+  }
+  
+  seleccionarJugador();
+  
