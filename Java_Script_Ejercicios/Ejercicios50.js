56b90be3da2b0a591a4b81ee7bf212ee208f4068
Wilson Delgado <wilsondelgadomoreno@gmai.com>
2024-12-13 17:38:17 -0500

    Ejercicio50

--- Java_Script_Ejercicios/Ejercicios50.js
+++ Java_Script_Ejercicios/Ejercicios50.js
@@ -2,3 +2,25 @@
 // muestre en pantalla la palabra Alarma si la variable presión es mayor a 200 o si la variable
 // temperatura es mayor a 100. En caso contrario, se debe mostrar en pantalla la palabra 
 // Normal.
+
+function verificar(temperatura, presion) {
+    if (presion > 200 || temperatura > 100) {
+      alert('Alarma');
+    } else {
+      alert('Normal');
+    }
+  }
+  
+  function colicitamos() {
+    let temperatura = parseFloat(prompt('Ingrese la temperatura:'));
+    let presion = parseFloat(prompt('Ingrese la presión:'));
+  
+    if (isNaN(temperatura) || isNaN(presion)) {
+      alert('Por favor ingrese valores válidos para temperatura y presión.');
+    } else {
+      verificar(temperatura, presion);
+    }
+  }
+  
+  colicitamos();
+  
