e59c1d682ea4901ed9776e797860de1829bf6592
Wilson Delgado <wilsondelgadomoreno@gmai.com>
2024-12-12 20:15:14 -0500

    Ejercicio33

--- Java_Script_Ejercicios/Ejercicios33.js
+++ Java_Script_Ejercicios/Ejercicios33.js
@@ -1,1 +1,12 @@
 // Calcular la edad de una madre en el momento que dio a luz a alguno de sus hijos
+
+function calcularEdadMadre() {
+    let añoNacimientoMadre = parseInt(prompt("Ingresa el año de nacimiento de la madre:"));
+    let añoNacimientoHijo = parseInt(prompt("Ingresa el año de nacimiento del hijo:"));
+    let edadMadreAlDarALuz = añoNacimientoHijo - añoNacimientoMadre;
+
+    return alert(`La madre tenía ${edadMadreAlDarALuz} años cuando dio a luz a su hijo.`);
+  }
+  
+  calcularEdadMadre();
+  
