dd0b9707d80ec51e3d89efbcd18fe19f98567d29
Wilson Delgado <wilsondelgadomoreno@gmai.com>
2024-12-12 20:46:02 -0500

    Ejercicio36

--- Java_Script_Ejercicios/Ejercicios36.js
+++ Java_Script_Ejercicios/Ejercicios36.js
@@ -2,3 +2,11 @@
 // bizcocho especial de chocolate. Por cada 100 gramos de harina hay que añadir 10 gramos 
 // de cacao y un puñado de nueces. Si quiero prepararlos con 20 gramos de chocolate. ¿La 
 // cantidad de gramos de harina para hacer el bizcocho es?
+
+const calcularHarina = (gramosCacao) => {
+    return (100 * gramosCacao) / 10;
+}
+const gramosCacao = 20;
+const gramosHarina = calcularHarina(gramosCacao);
+alert(`Para ${gramosCacao} gramos de cacao, necesitas ${gramosHarina} gramos de harina.`);
+  
