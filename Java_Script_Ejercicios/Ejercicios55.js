05670d7f3fe580b3aef366afa22edd482b90e3a8
Wilson Delgado <wilsondelgadomoreno@gmai.com>
2024-12-13 17:42:11 -0500

    Ejercicio55

--- Java_Script_Ejercicios/Ejercicios55.js
+++ Java_Script_Ejercicios/Ejercicios55.js
@@ -2,3 +2,36 @@
 // dependiendo de su tipo, sólo existen tres tipos de membresía, tipo A, tipo B y tipo C. Los 
 // descuentos son los siguientes: Tipo A 10% de descuento Tipo B 15% de descuento Tipo C
 // 20% de descuento.
+
+
+function calcular(precio, tipoMembresia) {
+    let descuento = 0;
+  
+    if (tipoMembresia === 'A') {
+      descuento = precio * 0.10; 
+    } else if (tipoMembresia === 'B') {
+      descuento = precio * 0.15;
+    } else if (tipoMembresia === 'C') {
+      descuento = precio * 0.20; 
+    } else {
+      alert('Tipo de membresía no válido');
+      return;
+    }
+  
+    let total = precio - descuento;
+    alert('Precio original: $' + precio + '\nDescuento: $' + descuento + '\nTotal a pagar: $' + total);
+  }
+  
+  function solicito() {
+    let precio = parseFloat(prompt('Ingrese el precio del helado:'));
+    let tipoMembresia = prompt('Ingrese el tipo de membresía (A, B, C):').toUpperCase();
+  
+    if (isNaN(precio) || precio <= 0) {
+      alert('Por favor ingrese un precio válido.');
+    } else {
+      calcular(precio, tipoMembresia);
+    }
+  }
+  
+  solicito();
+  
