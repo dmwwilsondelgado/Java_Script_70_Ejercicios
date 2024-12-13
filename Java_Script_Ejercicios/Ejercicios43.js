c71528d4eb72c487728479a0ceac9ee5a6105622
Wilson Delgado <wilsondelgadomoreno@gmai.com>
2024-12-13 17:23:52 -0500

    Ejercicio43

--- Java_Script_Ejercicios/Ejercicios43.js
+++ Java_Script_Ejercicios/Ejercicios43.js
@@ -1,2 +1,10 @@
 // Escriba una expresión regular que reconozca las cadenas de doble comillas. Debe permitir 
 // la presencia de comillas y caracteres escapados.
+
+const texto = 'Este es un "ejemplo" con "comillas escapadas \\"aquí\\"" en la cadena.';
+const regex = /"((?:[^"\\]|\\.)*)"/g;
+let match;
+
+while ((match = regex.exec(texto)) !== null) {
+  alert(match[0]);  
+}
