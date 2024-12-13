fdea23a5d1e2cd7e6498d519436af2f8b1781ebf
Wilson Delgado <wilsondelgadomoreno@gmai.com>
2024-12-12 21:10:27 -0500

    Ejercicio37

--- Java_Script_Ejercicios/Ejercicios37.js
+++ Java_Script_Ejercicios/Ejercicios37.js
@@ -1,2 +1,9 @@
 // <Un camión transporta 25 cajas de repuestos de carros. Si cada caja pesa 748 Kg ¿cuántos 
 // Kg transporta?>
+
+const calcularPesoTotal = (numeroCajas, pesoPorCaja) => numeroCajas * pesoPorCaja;
+alert(`<Un camión transporta 25 cajas de repuestos de carros. Si cada caja pesa 748 Kg ¿cuántos Kg transporta?>`);
+const numeroCajas = 25;
+const pesoPorCaja = 748;
+const pesoTotal = calcularPesoTotal(numeroCajas, pesoPorCaja);
+alert(`El camión transporta un peso total de ${pesoTotal} Kg.`);
