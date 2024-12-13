41e700824837865df4399d197e84c680ab6102e1
Wilson Delgado <wilsondelgadomoreno@gmai.com>
2024-12-13 17:58:06 -0500

    ewjercicio74

--- Java_Script_Ejercicios/Ejercicios74.js
+++ Java_Script_Ejercicios/Ejercicios74.js
@@ -1,2 +1,12 @@
 // escribir un programa que muestre el eco de todo lo que el usuario introduzca hasta que el 
 // usuario escriba “salir” que terminará
+
+let input;
+
+while (true) {
+  input = prompt("Escribe algo (o 'salir' para terminar):");
+  if (input === "salir") {
+    break;
+  }
+  alert(input);
+}
