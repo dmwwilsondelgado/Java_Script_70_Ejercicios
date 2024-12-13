23589be052e9db850fba85a34eecfa0eeab4d465
Wilson Delgado <wilsondelgadomoreno@gmai.com>
2024-12-13 17:48:03 -0500

    Ejercicio62

--- Java_Script_Ejercicios/Ejercicios62.js
+++ Java_Script_Ejercicios/Ejercicios62.js
@@ -1,1 +1,13 @@
 // Escribir un programa que pida al usuario una palabra y la muestre por pantalla 10 veces
+function mostrarPalabra() {
+    // Solicitar al usuario que ingrese una palabra
+    let palabra = prompt("Ingrese una palabra:");
+  
+    // Mostrar la palabra 10 veces
+    for (let i = 0; i < 10; i++) {
+      alert(palabra);
+    }
+  }
+  
+  mostrarPalabra();
+  
