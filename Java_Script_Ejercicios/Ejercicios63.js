f7cd319c52d7aaa126653c5da8413e5f6dae6190
Wilson Delgado <wilsondelgadomoreno@gmai.com>
2024-12-13 17:48:34 -0500

    Ejercicio63

--- Java_Script_Ejercicios/Ejercicios63.js
+++ Java_Script_Ejercicios/Ejercicios63.js
@@ -1,2 +1,15 @@
 // Escribir un programa que pregunte al usuario su edad y muestre por pantalla todos los 
 // años que ha cumplido (desde 1 hasta su edad)
+
+function mostrarAñosCumplidos() {
+    // Solicitar al usuario su edad
+    let edad = parseInt(prompt("¿Cuántos años tienes?"));
+  
+    // Mostrar todos los años que ha cumplido
+    for (let i = 1; i <= edad; i++) {
+      alert("Has cumplido " + i + " años");
+    }
+  }
+  
+  mostrarAñosCumplidos();
+  
