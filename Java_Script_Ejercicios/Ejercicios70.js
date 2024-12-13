2cd4ce60745a70ddd965ec2668b4d8b6e7be2542
Wilson Delgado <wilsondelgadomoreno@gmai.com>
2024-12-13 17:54:30 -0500

    Ejercicio70

--- Java_Script_Ejercicios/Ejercicios70.js
+++ Java_Script_Ejercicios/Ejercicios70.js
@@ -1,2 +1,17 @@
 // . Escribir un programa que almacene la cadena de caracteres contraseña en una variable, 
 // pregunte al usuario por la contraseña hasta que introduzca la contraseña correcta
+// Almacenar la contraseña correcta en una variable
+const contraseñaCorrecta = "contraseña";
+function veri() {
+  let contraseñaUsuario;
+  do {
+    contraseñaUsuario = prompt("Introduce la contraseña:");
+    if (contraseñaUsuario === contraseñaCorrecta) {
+      alert("Contraseña correcta. Acceso permitido.");
+    } else {
+      alert("Contraseña incorrecta. Intenta de nuevo.");
+    }
+  } while (contraseñaUsuario !== contraseñaCorrecta);
+}
+
+veri();
