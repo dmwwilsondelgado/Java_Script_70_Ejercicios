1d2515cf6373548603799502c64480bd3e6d0ae0
Wilson Delgado <wilsondelgadomoreno@gmai.com>
2024-12-13 17:19:07 -0500

    Ejercicio41

--- Java_Script_Ejercicios/Ejercicios41.js
+++ Java_Script_Ejercicios/Ejercicios41.js
@@ -1,2 +1,13 @@
 // ingrese una frase que lo identifique como programador de Software y luego mostrar esta 
 // frase invertida
+function invierte(frase) {
+    return frase.split('').reverse().join('');
+  }
+  function ingresa() {
+    var frase = prompt('Ingrese una frase que lo identifique como programador de Software:');
+    var fraseInvertida = invierte(frase);
+    alert('La frase invertida es: ' + fraseInvertida);
+  }
+  
+  ingresa();
+  
