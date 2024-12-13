1b951902e57a9dae7d280905f19945d91bc78640
Wilson Delgado <wilsondelgadomoreno@gmai.com>
2024-12-13 17:28:07 -0500

    Ejercicio45

--- Java_Script_Ejercicios/Ejercicios45.js
+++ Java_Script_Ejercicios/Ejercicios45.js
@@ -1,1 +1,22 @@
 // Determinar si un número es positivo y menor que 100.
+
+
+ function solicitamos() {
+    let numero = parseFloat(prompt('Ingrese un número x:'));
+  
+    if (isNaN(numero)) {
+      alert('Por favor ingrese un número válido.');
+    } else {
+      verificamos(numero);
+    }
+  }
+
+function verificamos(numero) {
+    if (numero > 0 && numero < 100) {
+      alert('El número es positivo y menor que 100.');
+    } else {
+      alert('El número no cumple con las condiciones.');
+    }
+}
+  solicitamos();
+  
