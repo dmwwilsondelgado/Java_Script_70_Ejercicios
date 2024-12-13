a4eef46d06efde9ce2ce37a523581ad26ca36489
Wilson Delgado <wilsondelgadomoreno@gmai.com>
2024-12-12 19:02:20 -0500

    Ejercicio27

--- Java_Script_Ejercicios/Ejercicios25.js
+++ Java_Script_Ejercicios/Ejercicios25.js
@@ -1,2 +1,12 @@
 // Una farmacia aplica el precio de los remedios el 10% de descuento, hacer un programa 
 // que ingresando el costo de los medicamentos calcule el descuento y el precio final.
+function calcularDescuento() {
+    alert("________Bienbenidos a dorguerias el Mewdicamento _________");
+    let costo = parseFloat(prompt("---------Ingresaa el costo de los medicamentos-----------"));
+    let descuento = costo * 0.10;
+    let precioFinal = costo - descuento;
+    alert("Descuento aplicado: " + descuento.toFixed(2) + "\nPrecio final: " + precioFinal.toFixed(2));
+  }
+  
+  calcularDescuento();
+  
--- Java_Script_Ejercicios/Ejercicios27.js
+++ Java_Script_Ejercicios/Ejercicios27.js
@@ -3,3 +3,32 @@
 // a. 55% del promedio final de sus calificaciones de los tres (3) parciales.
 // b. 30% de la calificación examen final.
 // c. 15% de la calificación trabajo final.
+function calcPromedio() {
+    let parcial1 = parseFloat(prompt("Ingresa la calificación del primer parcial:"));
+    let parcial2 = parseFloat(prompt("Ingresa la calificación del segundo parcial:"));
+    let parcial3 = parseFloat(prompt("Ingresa la calificación del tercer parcial:"));
+    
+    while (isNaN(parcial1) || isNaN(parcial2) || isNaN(parcial3)) {
+      alert("Por favor, ingresa todas las calificaciones correctamente.");
+      parcial1 = parseFloat(prompt("Ingresaa la calificación del primer parcial:"));
+      parcial2 = parseFloat(prompt("Ingresa la calificación del segundo parcial:"));
+      parcial3 = parseFloat(prompt("Ingresa la calificación del tercer parcial:"));
+    }
+  
+    let examenFinal = parseFloat(prompt("Ingreesa la calificación del examen final:"));
+    while (isNaN(examenFinal)) {
+      examenFinal = parseFloat(prompt("Por favor, ingresa una calificación válida para el examen final:"));
+    }
+  
+    let trabajoFinal = parseFloat(prompt("Ingreesa la calificación del trabajo final:"));
+    while (isNaN(trabajoFinal)) {
+      trabajoFinal = parseFloat(prompt("Por favorr, ingresa una calificación válida para el trabajo final:"));
+    }
+  
+    let promedioParciales = (parcial1 + parcial2 + parcial3) / 3;
+    let promedioFinal = (promedioParciales * 0.55) + (examenFinal * 0.30) + (trabajoFinal * 0.15);
+    alert("El promedio final es: " + promedioFinal.toFixed(2));
+  }
+  
+  calcPromedio();
+  
