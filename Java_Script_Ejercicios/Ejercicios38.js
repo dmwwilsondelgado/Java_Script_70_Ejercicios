e53c0eb0e144a73779df3ad669aea320935b0b89
Wilson Delgado <wilsondelgadomoreno@gmai.com>
2024-12-13 17:13:45 -0500

    Ejercicio38

--- Java_Script_Ejercicios/Ejercicios38.js
+++ Java_Script_Ejercicios/Ejercicios38.js
@@ -1,3 +1,16 @@
 // Tres estudiantes reunieron tapas de gaseosas para repartirlas y venderlas al final del año. 
 // Martín toma 2/3 del total, Jairo un cuarto del total, y Lorena se queda con el resto. ¿Qué 
 // parte le corresponde a Lorena?
+
+const calcularParteLorena = () => {
+    // Fracciones de Martín y Jairo
+    let parteMartin = 2 / 3;
+    let parteJairo = 1 / 4;
+    let sumaPartes = parteMartin + parteJairo;
+    
+    let parteLorena = 1 - sumaPartes;
+    alert(`A Lorena le corresponde ${parteLorena.toFixed(2)} del total de tapas de gaseosas.`);
+  }
+  
+  calcularParteLorena();
+  
