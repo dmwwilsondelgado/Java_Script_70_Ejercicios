2b7a9ae10159e8aff0e60a92192ca2cb9f2406f3
Wilson Delgado <wilsondelgadomoreno@gmai.com>
2024-12-13 17:55:25 -0500

    Ejercicio71

--- Java_Script_Ejercicios/Ejercicios71.js
+++ Java_Script_Ejercicios/Ejercicios71.js
@@ -1,2 +1,18 @@
 // Escribir un programa que pida al usuario un número entero y muestre por pantalla si es un 
 // número primo o no.
+
+let numero = parseInt(prompt("Introduce un número entero:"));
+
+function esPrimo(num) {
+  if (num <= 1) return false;
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) return false;
+  }
+  return true;
+}
+
+if (esPrimo(numero)) {
+  alert("El número es primo.");
+} else {
+  alert("El número no es primo.");
+}
