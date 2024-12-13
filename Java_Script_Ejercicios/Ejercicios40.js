6a31e19f3da8ccc2dcb9b3d9757f23a485eaaf73
Wilson Delgado <wilsondelgadomoreno@gmai.com>
2024-12-13 17:16:25 -0500

    Ejercicio40

--- Java_Script_Ejercicios/Ejercicios40.js
+++ Java_Script_Ejercicios/Ejercicios40.js
@@ -1,3 +1,16 @@
 // Una empresa que contrata personal requiere determinar la edad de las personas que 
 // solicitan trabajo, pero cuando se les realiza la entrevista sólo se les pregunta el año en que 
 // nacieron. Realice el código que representen el algoritmo para solucionar este problema.
+const calcularEdad = (anioNacimiento) => {
+    const anioActual = new Date().getFullYear();
+    return anioActual - anioNacimiento;
+  }
+  
+  const solicitamosedad = () => {
+    const anioNacimiento = parseInt(prompt('Ingrese su año de nacimiento:'));
+    const edad = calcularEdad(anioNacimiento);
+    alert(`Su edad es de ${edad} años.`);
+  }
+  
+  solicitamosedad();
+  
